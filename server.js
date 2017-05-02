219434d0a45685ed65490adcd1434ff16bf5f9a6
nmingaleev <nmingaleev@yandex.ru>
2017-05-02 12:43:18 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); //создает новое express-приложение
+
+hbs.registerPartials(__dirname + '/views/partials'); // Подключение Partials
+app.set('view engine', 'hbs'); // определяет view engine
+
+app.use((req, res, next) => { // Middleware
+  var now = new Date().toString();
+  var log = `${now}: ${req.ip} ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to srever.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => { // Еще один Middleware
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to our brand new website!'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to get the data'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About page"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
