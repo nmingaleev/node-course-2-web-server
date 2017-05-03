f65a26e8100bd5c19aec378063ce2aea9a2f130b
nmingaleev <nmingaleev@yandex.ru>
2017-05-03 12:46:43 -0700

    New project page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "My projects"
+  })
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
